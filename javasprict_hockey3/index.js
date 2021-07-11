6371c71fc7cbc445fda6f6828232c52d233dfe2e
根本洋次郎 <nemotoyojiro@nemotoyoujirounoMacBook-Air.local>
2021-07-11 13:32:16 +0900

    フォルダごとできるか

---
+++ javasprict_hockey3/index.js
@@ -0,0 +1,227 @@
+const $canvas = document.getElementById("myCanvas");
+const $ctx = $canvas.getContext("2d");
+
+let moveEasy = 10;
+let moveNormal = 15;
+let moveHard = 20;
+
+if($canvas.offsetLeft <= 0){
+    $canvas.width = 320;
+    $canvas.height = 400;
+    moveEasy = 5;
+    moveNormal = 8;
+    moveHard = 10;
+}
+
+let x = $canvas.width/2;
+let y = $canvas.height-30;
+
+let dx = 5;
+let dy = -5;
+let dxp = 5;
+
+const ballRadius = 10;
+let wid = $canvas.width;
+let hi = $canvas.height;
+let ballColor = 'skyblue';
+let paddleColor = 'blue';
+let paddleColorE = 'black';
+const paddleHeightM = 8;
+let paddleWidthM = 1.5*$canvas.width/8;
+const paddleHeightE = 8;
+let paddleWidthE = 1.5*$canvas.width/8;
+let paddleEMove = 20;
+let paddleXMW = (wid-paddleWidthM)/2;
+let paddleXMH = hi-paddleHeightM;
+let paddleXEW = (wid-paddleWidthE)/2;
+let paddleXEH = 0;
+let rightPressed = false;
+let leftPressed = false;
+let score = 0;
+
+//ボタン情報の取得あと文も
+const $doc = document;
+const $levels = $doc.getElementById('levels');
+const $level = $levels.querySelectorAll('[data-lev]');
+const tex = $doc.getElementById('tex')
+
+//ボタンを消す関数
+const reset = (a) => {
+    $level[a].style.display = 'none';
+};
+
+
+//操作方法の定義
+document.addEventListener("keydown",(e) => {keyDownHandler(e);});
+document.addEventListener("keyup", (e) => {keyUpHandler(e);});
+document.addEventListener("mousemove", (e) => mouseMoveHandler(e));
+document.addEventListener("touchmove", (e) => touchMoveHandler(e), {passive: false});
+
+const keyDownHandler = (e) => {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+};
+
+const keyUpHandler = (e) => {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+};
+
+const mouseMoveHandler = (e) => {
+    let relativeX = e.clientX - $canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < $canvas.width) {
+        paddleXMW = relativeX - paddleWidthM/2 ;
+    }
+};
+
+const touchMoveHandler = (e) => {
+    e.preventDefault();
+    let relativeXT = e.changedTouches[0].clientX - $canvas.offsetLeft;
+    if(relativeXT > 0 && relativeXT < $canvas.width) {
+        paddleXMW = relativeXT - paddleWidthE/2;
+    }
+}
+
+
+//ballを描く
+const drawball = () => {
+    $ctx.beginPath();
+    $ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    $ctx.fillStyle = '#A00';
+    $ctx.fill();
+    $ctx.closePath(); 
+};
+
+drawball();
+
+//paddleを描く
+const drawPaddleMy = () => {
+    $ctx.beginPath();
+    $ctx.rect(paddleXMW, paddleXMH, paddleWidthM, paddleHeightM);
+    $ctx.fillStyle = paddleColor;
+    $ctx.fill();
+    $ctx.closePath();
+};
+
+drawPaddleMy();
+
+//paddleを描く２
+const drawPaddleEn = () => {
+    $ctx.beginPath();
+    $ctx.rect(paddleXEW, paddleXEH, paddleWidthE, paddleHeightE);
+    $ctx.fillStyle = paddleColorE;
+    $ctx.fill();
+    $ctx.closePath();
+};
+
+drawPaddleEn();
+
+//合体.動かす関数
+const draw = () => {
+    $ctx.clearRect(0, 0, wid, hi);
+    drawball(); 
+    drawPaddleMy();
+    drawPaddleEn();
+
+    if(x + dx > wid - ballRadius || x + dx < 0 + ballRadius){
+        dx = -dx;
+    }
+
+    if(y +dy < 0 + ballRadius + paddleHeightE && (x +dx >= paddleXEW && x +dx <= paddleXEW + paddleWidthE)){
+        dy = -(Math.random()/8 + 1 )*dy;
+        dx = (Math.random()/8 + 1 )*dx;
+        score++;
+    }
+    if(y <= 0){
+        alert("YOU WIN !! (SCORE：" + score + ")");
+        document.location.reload();
+        x = $canvas.width/2;
+        y = $canvas.height-30;
+        paddleXMW = (wid-paddleWidthM)/2;
+        paddleXEW = (wid-paddleWidthE)/2;
+    }
+
+    if(y +dy > hi - ballRadius - paddleHeightM && (x + dx >= paddleXMW && x + dx <= paddleXMW + paddleWidthM)){
+        dy = -(Math.random()/8 + 1 )*dy;
+        dx = (Math.random()/8 + 1 )*dx;
+        score++;
+    }
+    else if(y >= hi ){
+        alert("GAMEOVER....(SCORE：" + score + ")");
+        document.location.reload();
+        x = $canvas.width/2;
+        y = $canvas.height-30;
+        paddleXMW = (wid-paddleWidthM)/2;
+        paddleXEW = (wid-paddleWidthE)/2;
+    }
+
+    if(rightPressed && paddleXMW < wid-paddleWidthM){
+        paddleXMW += 15;
+    }
+    else if(leftPressed && paddleXMW > 0) {
+        paddleXMW -= 15;
+    }
+    
+    if(paddleXEW + paddleWidthE*3/4 < x){
+        paddleXEW += paddleEMove;
+    }
+    else if(paddleXEW + paddleWidthE*1/4> x ){
+        paddleXEW -= paddleEMove;
+    }
+
+    x += dx;
+    y += dy;
+
+    requestAnimationFrame(draw);    
+
+};
+
+//難易度選択する関数
+const handleClick = (e) => {
+    e.preventDefault();
+
+    const $target = e.target;
+    const targetVal = $target.dataset.lev;
+
+    let index = 0;
+    while(index < $level.length){
+        reset(index);
+        index++;
+    }
+
+    if(targetVal === '0'){
+        paddleEMove = moveEasy;
+        tex.innerText = '難易度：Easy';
+    }
+    else if(targetVal === '1'){
+        paddleEMove = moveNormal;
+        tex.innerText = '難易度：Normal';
+    }
+    else{
+        paddleEMove = moveHard;
+        paddleWidthE = 1.5*paddleWidthE;
+        tex.innerText = '難易度：Hard';
+    }
+    window.alert('Are you ready??');
+    draw(); 
+};
+
+//クリックされたら実行する
+let index = 0;
+    while(index < $level.length){
+        $level[index].addEventListener('click', (e) => handleClick(e));
+        index++;
+    }
+
+
+
+
+
